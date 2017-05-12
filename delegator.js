2ac1d45611fc635aa14a7ef11989296579e4c30c
uyeong <uyeong21c@gmail.com>
2017-05-12 17:19:45 +0900

    Write createDelegator function, on method.

--- delegator.js
+++ delegator.js
@@ -7,17 +7,72 @@
         factory(root);
     }
 })(this, function(exports) {
-    var Delegator = function() {
-        this.init.apply(this, arguments);
-    };
+    function createDelegator(original, options) {
+        original = original || document.body;
+        options = options || {};
+        options.capture = !!options.capture;
+        options.passive = !!options.passive;
 
-    Delegator.prototype = {
-        constructor: Delegator,
+        var delegates = {};
+        var listeners = {};
 
-        init: function() {
-            // Write initialize code.
+        function createDelegateListener(type) {
+            return function (event) {
+                var target = event.target;
+                var targetListeners;
+                var selector;
+                var i, n;
+
+                for (selector in listeners[type]) {
+                    if (listeners[type].hasOwnProperty(selector)) {
+                        if (target.matches(selector) || target.closest(selector)) {
+                            event.delegateTarget = original;
+                            targetListeners = listeners[type][selector];
+
+                            for (i = 0, n = targetListeners.length; i < n; i++) {
+                                targetListeners[i](event);
+                            }
+                        }
+                    }
+                }
+            };
         }
-    };
 
-    exports.Delegator = Delegator;
+        return {
+            target: original,
+            options: options,
+
+            on: function(type, selector, listener) {
+                if (!listeners[type]) {
+                    listeners[type] = {};
+                }
+
+                if (!listeners[type][selector]) {
+                    listeners[type][selector] = [];
+                }
+
+                listeners[type][selector].push(listener);
+
+                if (!delegates[type]) {
+                    delegates[type] = createDelegateListener(type);
+                    original.addEventListener(type, delegates[type], options);
+                }
+            },
+
+            clear() {
+                var type;
+
+                for (type in delegates) {
+                    if (delegates.hasOwnProperty(type)) {
+                        original.removeEventListener(type, delegates[type], options);
+                    }
+                }
+
+                listeners = {};
+                delegates = {};
+            }
+        };
+    }
+
+    exports.createDelegator = createDelegator;
 });
--- tests.js
+++ tests.js
@@ -1,16 +1,73 @@
-describe('Test of the Delegator.', function() {
-    before(function() {
+let original;
+let button;
+let input;
+let delegator;
+
+describe('Test of the delegator.', function() {
+    before(() => {
         window.__html__ = window.__html__ || {};
         document.body.innerHTML = window.__html__.fixture;
+
+        original = document.querySelector('.wrap');
+        button = original.querySelector('.action-btn');
+        input = original.querySelector('.action-input');
+        delegator = createDelegator(original);
     });
 
-    describe('Something Test', function() {
-        it('should be ...', function() {
-            assert.isNotNull(document.querySelector('.wrap'));
+    afterEach(() => {
+        delegator.clear();
+    });
+
+    describe('특정 엘리먼트를 타겟으로 이벤트를 위임할 수 있다.', () => {
+        it('.on()를 통해 이벤트를 위임할 수 있다.', function() {
+            // Given
+            const spyClickEvent = sinon.spy();
+            const spyTouchEvent = sinon.spy();
+
+            // When
+            delegator.on('click', '.action-btn', spyClickEvent);
+            delegator.on('keyup', '.action-input', spyTouchEvent);
+
+            button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+            input.dispatchEvent(new KeyboardEvent('keyup', {bubbles: true}));
+
+            // Then
+            assert.equal(spyClickEvent.callCount, 1);
+            assert.equal(spyTouchEvent.callCount, 1);
         });
 
-        it('should be ...', function() {
-            assert.isFunction(Delegator);
+        it('위임 이벤트 리스너의 인자를 통해 오리지널 엘리먼트를 알 수 있다.', () => {
+            // Given
+            const spy = sinon.spy();
+
+            // When
+            delegator.on('click', '.action-btn', spy);
+
+            button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+
+            // Then
+            assert.equal(spy.callCount, 1);
+            assert.equal(spy.args[0][0].target, button);
+            assert.equal(spy.args[0][0].delegateTarget, original);
+        });
+
+        it('캡쳐링 단계에서 이벤트를 위임할 수 있다.', () => {
+            // Given
+            const spyClickEvent1 = sinon.spy();
+            const spyClickEvent2 = sinon.spy((e) => e.stopPropagation());
+            const capturing = createDelegator(original, {capture: true});
+
+            // When
+            delegator.on('click', '.action-btn', spyClickEvent1);
+            capturing.on('click', '.action-btn', spyClickEvent2);
+
+            button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+
+            // Then
+            assert.equal(spyClickEvent1.callCount, 0);
+            assert.equal(spyClickEvent2.callCount, 1);
+
+            capturing.clear();
         });
     });
 });
