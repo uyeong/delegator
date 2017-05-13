1f1a3e2ed4b677a8dbd79e0ffbe473b2185ccf5c
UYEONG <uyeong21c@gmail.com>
2017-05-13 13:04:04 +0900

    Added force capture case

--- delegator.js
+++ delegator.js
@@ -7,6 +7,26 @@
         factory(root);
     }
 })(this, function(exports) {
+    function captureForType(type) {
+        // https://www.w3.org/TR/DOM-Level-2-Events/events.html#Events-eventgroupings-htmlevents
+        return ['blur', 'error', 'focus', 'load', 'resize', 'scroll'].indexOf(type) !== -1;
+    }
+
+    function merge(/* args */) {
+        var target = arguments[0];
+        var i, n, key;
+
+        for(i = 1, n = arguments.length; i < n; i++) {
+            for (key in arguments[i]) {
+                if (arguments[i].hasOwnProperty(key)) {
+                    target[key] = arguments[i][key];
+                }
+            }
+        }
+
+        return target;
+    }
+
     function createDelegator(original, options) {
         original = original || document.body;
         options = options || {};
@@ -52,7 +72,11 @@
 
                 if (!delegates[type]) {
                     delegates[type] = createDelegateListener(type);
-                    original.addEventListener(type, delegates[type], options);
+                    original.addEventListener(
+                        type,
+                        delegates[type],
+                        captureForType(type) ? merge({}, options, {capture: true}) : options
+                    );
                 }
             },
 
@@ -87,7 +111,11 @@
 
                 for (type in delegates) {
                     if (delegates.hasOwnProperty(type)) {
-                        original.removeEventListener(type, delegates[type], options);
+                        original.removeEventListener(
+                            type,
+                            delegates[type],
+                            captureForType(type) ? merge({}, options, {capture: true}) : options
+                        );
                     }
                 }
 
